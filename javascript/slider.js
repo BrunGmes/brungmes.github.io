419ee2bbe47b05edd9fc06ca5cdaab08e210be48
Bruno Gmes <bhsgomes@hotmail.com>
2024-07-11 22:13:23 +0100

    Upload website

---
+++ javascript/slider.js
@@ -0,0 +1,25 @@
+	$(".slider").slick({
+	dots: true,
+	fade: true,
+	autoplay: true,
+	speed: 1000,
+	cssEase: 'linear'
+	})
+
+ 	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.slider-nav'
+	})
+	
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: true,
+	  centerMode: true,
+	  focusOnSelect: true
+	});
+				
